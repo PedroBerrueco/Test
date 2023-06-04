23249b53785154bc7a484e7a86dcf9607b8bb829
Pedro Ángel Berrueco Amores <paberrueco@knowmadmood.com>
2023-06-04 18:10:09 +0200

    pruebas

---
+++ src/test/java/com/utad_prueba/ControladorTest.java
@@ -0,0 +1,19 @@
+package com.utad_prueba;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ControladorTest {
+
+	@Test
+	public void testSumar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRestar() {
+		fail("Not yet implemented");
+	}
+
+}
